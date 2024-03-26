c0db5eb23f51a43b613285b484419c737c3c91ba
Lydia Qin <lydiaqin@college.harvard.edu>
2024-03-26 07:12:16 -0400

    graphs

--- graphs.py
+++ graphs.py
@@ -1,10 +1,12 @@
 import numpy as np
 import strassen
 
+v = 1024
+
 def make_graph(p):
-    A = np.matrix(np.random.rand(1024, 1024))
-    for i in range(1024):
-        for j in range(1024):
+    A = np.matrix(np.random.rand(v, v))
+    for i in range(v):
+        for j in range(v):
             if A[i, j] < p:
                 A[i, j] = 1
             else: 
@@ -12,17 +14,18 @@ def make_graph(p):
     return A
 
 def count_triangles(A):
-    A2 = strassen.strassen(A, A, 1024)
-    A3 = strassen.strassen(A2, A, 1024)
+    A2 = strassen.strassen(A, A, v)
+    A3 = strassen.strassen(A2, A, v)
     sum = 0
-    for i in range(1024):
-        sum = sum + A[i, i]
-    return sum
+    for i in range(v):
+        sum = sum + A3[i, i]
+    return (sum / 6.0)
 
 def main():
-    A = make_graph(0.01)
-    count = count_triangles(A)
-    print("p = 0.01: ", count)
+    for p in [0.01, 0.02, 0.03, 0.04, 0.05]:
+        A = make_graph(p)
+        count = count_triangles(A)
+        print("p =", p, "-", count)
 
     # for i in range(1024):
     #     for j in range(1024):
