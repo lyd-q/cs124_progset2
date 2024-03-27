1cc307cbd3affd753331b60f3d02ff8577afc81f
cyang19 <cyang@college.harvard.edu>
2024-03-26 21:59:25 -0400

    optimized strassen

--- experiments.py
+++ experiments.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 naivelist = []
 strassenlist = []
-nvals = list(range(4, 10))
+nvals = list(range(4, 15))
 for n in nvals:
     #generate a matrix of size n:
     matrix1 = np.matrix(np.zeros((n,n)))
@@ -54,46 +54,3 @@ plt.legend()  # Adding legend
 
 
 plt.show()
-
-# #generate a list of random matrices
-# # each dimension generate 10 matrices -> 500, 1000, 1500 
-# matrixlist1 = [[0]*10]*3
-# matrixlist2 = [[0]*10]*3
-# i = 0 
-# for n in [250, 500, 750]:
-#     for j in range(5):
-#         matrixlist1[i][j] = np.matrix(np.zeros((n, n)))
-#         matrixlist2[i][j] = np.matrix(np.zeros((n, n)))
-#         for x in range(n):
-#             for y in range(n):
-#                 matrixlist1[i][j][x,y] =  np.random.choice([0,1])
-#                 matrixlist2[i][j][x,y] =  np.random.choice([0,1])
-#     i += 1
-
-# #experiments
-# def experiments():
-#     crosspoints = list(range(256, 512, 50)) #put crossover points here
-#     mult_time = [[0]*len(crosspoints)]*3 #3 sublists, each with the crosspoint times 
-#     #these are all the crossover points
-#     for i in range(3):
-#         for cross in crosspoints:
-#             sum = 0
-#             for j in range(5):
-#                 tic = time.time()
-#                 strassen.strassen(matrixlist1[i][j], matrixlist2[i][j], cross)
-#                 toc = time.time()
-#                 sum += toc - tic
-#             print(sum/5)
-#             #mult_time[i][cross] = sum/10
-#     return(mult_time)
-    
-    
-#     # for matrix in matrixlist:
-#     #     for cross in range(1, crosspoints):
-#     #         tic = time.perf_counter()
-#     #         strassen.strassen(A, B, cross)
-#     #         toc = time.perf_counter()
-#     #         mult_time[cross] = (toc - tic)
-
-    
-# print(experiments())
--- strassen.py
+++ strassen.py
@@ -1,4 +1,9 @@
 # strassen file - write as own function that takes in a matrix 
+
+#read in the file 
+
+
+
 # take in n and crossover value, check if n is less than crossover value
 # if n is less than crossover value, run matmul
 import numpy as np
@@ -20,28 +25,62 @@ def strassen(M1, M2, n0):
     if n >= n0:
          #if n is even, we don't need to pad 
         if n%2 == 0: 
-            A = M1[:n/2, :n/2]
-            B = M1[:n/2, n/2:]
-            C = M1[n/2:, :n/2]
-            D = M1[n/2:, n/2:]
-            E = M2[:n/2, :n/2]
-            F = M2[:n/2, n/2:]
-            G = M2[n/2:, :n/2]
-            H = M2[n/2:, n/2:]
-
-            P1 = strassen(A, F-H, n0) 
-            P2 = strassen(A+B, H, n0)
-            P3 = strassen(C+D, E, n0)
-            P4 = strassen(D, G-E, n0)
-            P5 = strassen(A+D, E+H, n0)
-            P6 = strassen(B-D, G+H, n0)
-            P7 = strassen(C-A, E+F, n0)
 
             ret = np.matrix(np.zeros((n, n)))
-            ret[:n/2, :n/2] = P4 + P5 + P6 - P2
-            ret[:n/2, n/2:] = P1 + P2
-            ret[n/2:, :n/2] = P3 + P4
-            ret[n/2:, n/2:] = P1 - P3 + P5 + P7
+
+            #P1 
+            X1 = M1[:n/2, :n/2]
+            X2 = M2[:n/2, n/2:] - M2[n/2:, n/2:]
+            P = strassen(X1, X2, n0)
+            
+            ret[:n/2, n/2:] += P
+            ret[n/2:, n/2:] += P
+
+            #P2 
+            X1 = M1[:n/2, :n/2] + M1[:n/2, n/2:]
+            X2 = M2[n/2:, n/2:]
+            P = strassen(X1, X2, n0) 
+
+            ret[:n/2, :n/2] -= P
+            ret[:n/2, n/2:] += P 
+
+            #P3 
+            X1 = M1[n/2:, :n/2] + M1[n/2:, n/2:]
+            X2 = M2[:n/2, :n/2]
+            P = strassen(X1, X2, n0)
+
+            ret[n/2:, :n/2] += P
+            ret[n/2:, n/2:] -= P
+
+            #P4 
+            X1 = M1[n/2:, n/2:]
+            X2 = M2[n/2:, :n/2] - M2[:n/2, :n/2]
+            P = strassen(X1, X2, n0)
+
+            ret[:n/2, :n/2] += P
+            ret[n/2:, :n/2] += P
+
+            #P5 
+            X1 = M1[:n/2, :n/2] + M1[n/2:, n/2:]
+            X2 = M2[:n/2, :n/2] + M2[n/2:, n/2:] 
+            P = strassen(X1, X2, n0)
+
+            ret[:n/2, :n/2] += P
+            ret[n/2:, n/2:] += P
+
+            #P6 
+            X1 = M1[:n/2, n/2:] - M1[n/2:, n/2:]
+            X2 = M2[n/2:, :n/2] + M2[n/2:, n/2:]
+            P = strassen(X1, X2, n0)
+
+            ret[:n/2, :n/2] += P
+
+            #P7 
+            X1 = M1[n/2:, :n/2] - M1[:n/2, :n/2]
+            X2 = M2[:n/2, :n/2] + M2[:n/2, n/2:]
+            P = strassen(X1, X2, n0)
+
+            ret[n/2:, n/2:] += P
 
             return(ret)
         #if n is odd, we need to pad, and then recursively call again
@@ -54,8 +93,9 @@ def strassen(M1, M2, n0):
     else: 
         return matmul(M1, M2)
 
-# test1 = np.matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
-# test2 = np.matrix([[1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0]])
-
-# print(strassen(test1, test2, 3))
+#test1 = np.matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+#test2 = np.matrix([[1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0]])
+#print(strassen(test1, test2, 3))
 # print(test1[:3, :3])
+
+
